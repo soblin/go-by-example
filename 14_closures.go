cabde8d2af1e8ca39974f5da129a99142fa5e193
soblin <hilo.soblin@gmail.com>
2021-07-25 21:30:07 -0700

    added 14

---
+++ 14_closures.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	nextInt2 := intSeq()
+	fmt.Println(nextInt2())
+	fmt.Println(nextInt2())
+}
